f797232a7b998c415830470326ac6c10ea5dde8b
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-01 13:56:15 +0100

    add properties

--- shape.py
+++ shape.py
@@ -1,10 +1,16 @@
 class Shape:
-    def __init__(self, x, y):
-        if not isinstance(x, (int, float)) or not isinstance (y, (int, float)):
+    def __init__(self, x, y): 
+        if not isinstance(x, (int, float)) or not isinstance (y, (int, float)): #error handling, making sure only numeriva values
             raise TypeError ('x and y must be numeric values')
-        self._x = x
+        self._x = x 
         self._y = y
 
-    @property
+    @property #making them only read only
     def x(self) -> float:
-        
+        return self._x 
+    
+    @property
+    def y(self) -> float:
+        return self._y
+    
+    
