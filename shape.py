5bf09ea17a9d70e22a57d3e706aa177b6f68341f
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-01 13:52:11 +0100

    new code 1/11

--- shape.py
+++ shape.py
@@ -1,6 +1,10 @@
 class Shape:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    
+        if not isinstance(x, (int, float)) or not isinstance (y, (int, float)):
+            raise TypeError ('x and y must be numeric values')
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self) -> float:
+        
