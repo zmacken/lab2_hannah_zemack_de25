b515aea90db53e86de8ca4e06af5238d72121286
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-03 15:38:31 +0100

    rectangle firs code

---
+++ rectangle.py
@@ -0,0 +1,22 @@
+from shape import Shape
+import math
+
+class Rectangle(Shape):
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y)
+        if not isinstance(w, (int,float)) or not isinstance(h, (int, float)):
+            raise TypeError ('height and widht must be a numeric value')
+        self._w = w
+        self._h= h
+    
+    @property
+    def w(self):
+        return self._w
+    
+    @property
+    def h(self):
+        return self._h
+    
+    @property
+    def area(self) -> float:
+        return 
