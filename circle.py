b900699137aa033c972fdebb5313b217fabbcdf3
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-03 20:40:31 +0100

    Update circle.py

--- circle.py
+++ circle.py
@@ -20,6 +20,15 @@ class Circle(Shape):
     def perimeter(self) -> float:
         return 2 * self._r *math.pi
     
+    def __eq__(self, other):
+        if not isinstance(other, Circle):
+            return False
+        
+        if self.area == other.area:
+            return True
+        else:
+            return False
+    
     def __repr__(self):
         base = super().__repr__()
         return f'{base[:-1]}, radius = {self._r}'
