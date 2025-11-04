c6949cfaccc3fcdbe1ec3a4112c347173113fc04
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-04 19:26:23 +0100

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -7,13 +7,13 @@ class Shape2Dplotter: #define a class to use for 2d plotting
 
     def add_shape(self, shape: Shape): #function to add to list shapes
         if not isinstance(shape, Shape):
-            raise TypeError("Only Shape instances can be added.")
+            raise TypeError("Only Shape instances can be added")
         self.shapes.append(shape)
 
     def plot(self):
         fig, ax = plt.subplots() #create subplots
-        for shape in self.shapes: #
-            shape.draw(ax)  # every figure knows hoe to write it self
+        for shape in self.shapes: 
+            shape.draw(ax)  # every figure knows how to write it self
         ax.set_aspect('equal') #equal scaling for x and y
         ax.autoscale_view() #automatically adjust view to fit all shapes
         plt.show() #display the plot window
