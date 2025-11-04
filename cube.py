a64b465c96cbc1e15c691e51cc0f131f25c1f618
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-04 19:49:54 +0100

    Create cube.py

---
+++ cube.py
@@ -0,0 +1,25 @@
+from shape import Shape
+
+class Cube(Shape):
+    def __init__(self, x, y, side):
+        super().__init__(x, y)
+        if not isinstance(side, (int,float)): #error handling to check if side is number
+            raise TypeError ('side must be a numeric value')
+        self._side = side
+
+    @property
+    def side(self):
+        return self._side
+    
+    @property
+    def volume(self):
+        return self._side **3
+    
+    def __eq__(self, other):
+        if not isinstance(other, Cube): #check if other object is cube
+            return False
+        
+        if self.volume == other.volume: #check if volume are the same
+            return True
+        else:
+            return False
