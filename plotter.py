132fdedfb9ec31bcf6fdb56a15b5c7ba9084e8b3
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-04 12:47:20 +0100

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,3 +1,10 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle as mplCircle, Rectangle as mplRectangle
 
+class Shape2Dplotter: #define a class to use for 2d plotting
+    def __init__(self): #initialize an empty list to store shapes
+        self.shapes = [] #list to hold all shapes
+    
+    def add_shapes(self, shape): #function to add to list shapes
+        self.shapes.append(shape) #add to list shapes
+    
