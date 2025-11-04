6d42453cf22589ef90bf48917491f59cb944d59c
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-04 20:55:26 +0100

    updated

--- test_circle.py
+++ test_circle.py
@@ -6,3 +6,14 @@ def test_circle_area():
     r = 1
     c = Circle(0, 0, r)
     assert round(c.area) == round(math.pi * r**2)
+
+def test_greater_than():
+    r = 1
+
+# print(circle1 == circle2)  # True
+# print(circle2 == rectangle)  # False
+# circle1.translate(5, 3) # moves its center 5 points in x and 3 points in y
+# #circle1.translate("THREE", 5)  # raise TypeError with an appropriate message
+# circle3 = Circle(radius=3) # a circle with center 0,0 with radius 3
+# circle3 >= circle1 # True
+# rectangle2 = Rectangle(width=3, height="5") # raise TypeError
