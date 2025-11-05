a0d95f3b1f7ae4f7def9472cf709e1a2198a4f85
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-05 13:27:52 +0100

    new plotter and test

--- plotter.py
+++ plotter.py
@@ -13,7 +13,10 @@ class Shape2Dplotter: #define a class to use for 2d plotting
     def plot(self):
         fig, ax = plt.subplots() #create subplots, fig needs to be there to access ax
         for shape in self.shapes: 
-            shape.draw(ax)  # every figure knows how to write it self
+            try:
+                shape.draw(ax)  # every figure knows how to write it self
+            except AttributeError:
+                print ('only 2d objects can be plotted')
         ax.set_aspect('equal') #equal scaling for x and y
         ax.autoscale_view() #automatically adjust view to fit all shapes
         plt.grid(True, color='green', linewidth = .1)
