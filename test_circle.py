a424565a4ad196b4a3b34bf190af97314d5ddbfb
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-05 11:13:35 +0100

    Update test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -40,14 +40,4 @@ def test_greater_or_equal():
 def test_translate():
     r= 1
     c1 = Circle(0,0,r)
-    
 
-
-
-# print(circle1 == circle2)  # True
-# print(circle2 == rectangle)  # False
-# circle1.translate(5, 3) # moves its center 5 points in x and 3 points in y
-# #circle1.translate("THREE", 5)  # raise TypeError with an appropriate message
-# circle3 = Circle(radius=3) # a circle with center 0,0 with radius 3
-# circle3 >= circle1 # True
-# rectangle2 = Rectangle(width=3, height="5") # raise TypeError
