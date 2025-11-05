c395eb056f2cf047c0c085a6a905340db88f8b8e
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-05 11:20:29 +0100

    pytest

--- test_circle.py
+++ test_circle.py
@@ -7,37 +7,38 @@ def test_circle_area():
     c = Circle(0, 0, r)
     assert round(c.area) == round(math.pi * r**2)
 
-def test_greater_than():
+def test_greater_than_circle():
     r = 1
     c1 = Circle(0,0,r*2)
     c2 = Circle(0, 0, r)
     assert c1 > c2
 
-def test_less_than():
+def test_less_than_circle():
     r = 1
     c1 = Circle(0,0,r)
     c2 = Circle(0, 0, r*2)
     assert c1 < c2
 
-def test_equal():
+def test_equal_circle():
     r=1
     c1 = Circle (0,0,r)
     c2 = Circle (0,0,r)
     assert c1 == c2
 
-def test_less_or_equal():
+def test_less_or_equa_circlel():
     r=1
     c1 = Circle(0,0,r)
     c2 = Circle(0,0,r)
     assert c1<=c2
 
-def test_greater_or_equal():
+def test_greater_or_equal_circle():
     r=1
     c1 = Circle(0,0,r)
     c2 = Circle(0,0,r)
     assert c1>=c2
 
-def test_translate():
+def test_translate_circle():
     r= 1
     c1 = Circle(0,0,r)
+    assert c1.translate(1, 1)
 
--- test_rectangle.py
+++ test_rectangle.py
@@ -7,37 +7,43 @@ def test_rectangle_area():
     r1 = Rectangle(0, 0, w, h)
     assert r1.area == 1
 
-def test_greater_than():
+def test_greater_than_rectangle():
     w = 1
     h = 1
     r1 = Rectangle(0, 0, 2*w, 2*h)
     r2 = Rectangle(0, 0, w, h)
     assert r1 > r2
 
-def test_less_than():
+def test_less_than_rectangle():
     w = 1
     h = 1
     r1 = Rectangle(0, 0, w, h)
     r2 = Rectangle(0, 0, 2*w, 2*h)
     assert r1 < r2
 
-def test_equal():
+def test_equal_rectangle():
     w = 1
     h = 1
     r1 = Rectangle(0, 0, w, h)
     r2 = Rectangle(0, 0, w, h)
     assert r1 == r2
 
-def test_less_or_equal():
+def test_less_or_equal_rectangle():
     w = 1
     h = 1
     r1 = Rectangle(0, 0, w, h)
     r2 = Rectangle(0, 0, w, h)
     assert r1 <= r2
 
-def test_greater_or_equal():
+def test_greater_or_equal_rectangle():
     w = 1
     h = 1
     r1 = Rectangle(0, 0, w, h)
     r2 = Rectangle(0, 0, w, h)
     assert r1 >= r2
+
+def test_translate_rectangle():
+    w = 1
+    h = 1
+    r1 = Rectangle(0, 0, w, h)
+    assert r1.translate(1, 1)
