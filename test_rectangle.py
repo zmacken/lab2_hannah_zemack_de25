db09195b48d53d6862e0842839d31fb761bda894
Hannah Zemack <hannah.zemack@gmail.com>
2025-11-06 10:33:22 +0100

    added test

--- test_rectangle.py
+++ test_rectangle.py
@@ -13,6 +13,12 @@ def test_perimeter_rectangle():
     r1 = Rectangle(0, 0, w, h)
     assert r1.perimeter == 4
 
+def test_rectangle_is_square():
+    w = 2
+    h = 2
+    r1 = Rectangle(0, 0, w, h)
+    assert r1.is_square() == True
+
 def test_greater_than_rectangle():
     w = 1
     h = 1
@@ -34,6 +40,13 @@ def test_equal_rectangle():
     r2 = Rectangle(0, 0, w, h)
     assert r1 == r2
 
+def test_not_equal_rectangle():
+    w = 1
+    h = 1
+    r1 = Rectangle(0, 0, w, h)
+    r2 = Rectangle(0, 0, 2*w, 2*h)
+    assert r1 != r2
+
 def test_less_or_equal_rectangle():
     w = 1
     h = 1
@@ -54,3 +67,17 @@ def test_translate_rectangle():
     r1 = Rectangle(0, 0, w, h)
     r1.translate(1, 1)
     assert r1.x == 1 and r1.y == 1
+
+def test_invalid_width():
+    with pytest.raises(TypeError):
+        Rectangle(0,0,"hej",2)
+
+def test_invalid_height():
+    with pytest.raises(TypeError):
+        Rectangle(0,0,2,"hej")
+
+def test_rectangle_str_and_repr():
+    r = Rectangle(0,0,1,1)
+    assert "Rectangle" in str(r)
+    assert "Rectangle" in repr(r)
+
